f1faed57d6d5f324e1beeb36b26ac0f1538159ee
Krupa Dhruva <krupadhruva@gmail.com>
2020-09-20 10:03:38 -0700

    Implement rectangle class + driver

---
+++ Miscellaneous/src/P7_Dhruva_Krupa_RectangleDriver.java
@@ -0,0 +1,15 @@
+import gpdraw.SketchPad;
+
+public class P7_Dhruva_Krupa_RectangleDriver {
+    public static void main(String[] args) {
+        SketchPad pad = new SketchPad(800, 600);
+
+        Rectangle rect1 = new Rectangle(0, 0, 240, 240, pad);
+        Rectangle rect2 = new Rectangle(80, 0, 80, 240, pad);
+        Rectangle rect3 = new Rectangle(0, 80, 240, 80, pad);
+
+        rect1.draw();
+        rect2.draw();
+        rect3.draw();
+    }
+}
