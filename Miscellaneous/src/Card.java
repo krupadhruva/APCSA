14d95f262236033f43c5480a4d9c0e36082be0fd
Krupa Dhruva <krupadhruva@gmail.com>
2021-01-10 18:58:39 -0800

    Selection Sort: Finds the max/min value of the given data and swaps with the entry currently sitting in the current location of max/min found.

---
+++ Miscellaneous/src/Card.java
@@ -0,0 +1,158 @@
+/*
+ * Name: Krupa Dhruva
+ * Date: December 02, 2020
+ * Period: 7
+ * Time Taken: 60 minutes
+ *
+ * Lab Reflection:
+ * Details are in class Deck
+ */
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.PrimitiveIterator;
+import java.util.Random;
+
+/** Card.java <code>Card</code> represents a basic playing card. */
+public class Card implements Comparable<Card>, Serializable {
+    // Source: https://mkyong.com/java/how-to-read-and-write-java-object-to-a-file/
+    // To ensure we read the same version from saved file
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * String value that holds the symbol of the card. Examples: "A", "Ace", "10", "Ten", "Wild",
+     * "Pikachu"
+     */
+    private final String symbol;
+
+    /** int value that holds the value this card is worth */
+    private final int value;
+
+    /** boolean value that determines whether this card is face up or down */
+    private boolean isFaceUp;
+
+    /**
+     * Creates a new <code>Card</code> instance.
+     *
+     * @param symbol a <code>String</code> value representing the symbol of the card
+     * @param value an <code>int</code> value containing the point value of the card
+     */
+    public Card(String symbol, int value) {
+        /* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+        this(symbol, value, false);
+    }
+
+    /**
+     * Underlying constructor allowing state for all attributes
+     *
+     * @param symbol a <code>String</code> value representing the symbol of the card
+     * @param value an <code>int</code> value containing the point value of the card
+     * @param isFaceUp a <code>boolean</code> value indicating if card is facing up
+     */
+    public Card(String symbol, int value, boolean isFaceUp) {
+        this.symbol = symbol.toUpperCase();
+        this.value = value;
+        this.isFaceUp = isFaceUp;
+    }
+
+    /**
+     * Getter method to access this <code>Card</code>'s symbol.
+     *
+     * @return this <code>Card</code>'s symbol.
+     */
+    public String getSymbol() {
+        /* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+        return symbol;
+    }
+
+    /**
+     * Gets the value of the card
+     *
+     * @return Value of card
+     */
+    public int getValue() {
+        /* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+        return value;
+    }
+
+    /**
+     * Returns the state of the card face
+     *
+     * @return True if card is facing up
+     */
+    public boolean isFaceUp() {
+        return isFaceUp;
+    }
+
+    /**
+     * Set the face state of the card
+     *
+     * @param state True is card face is up
+     */
+    public void setFaceUp(boolean state) {
+        isFaceUp = state;
+    }
+
+    /**
+     * Returns whether or not this <code>Card</code> is equal to another
+     *
+     * @return whether or not this Card is equal to other.
+     */
+    @Override
+    public boolean equals(Object other) {
+        /* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+        return other instanceof Card
+                && compareTo((Card) other) == 0
+                && isFaceUp() == ((Card) other).isFaceUp();
+    }
+
+    /**
+     * Returns this card as a String. If the card is face down, "X" is returned. Otherwise the
+     * symbol of the card is returned.
+     *
+     * @return a <code>String</code> containing the symbol or and X, depending on whether the card
+     *     is face up or down.
+     */
+    @Override
+    public String toString() {
+        /* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+        return getSymbol();
+    }
+
+    /**
+     * Implements method from interface Comparable on Card type
+     *
+     * @param other The card to compare against
+     * @return Difference between current card value and the other card value
+     */
+    @Override
+    public int compareTo(Card other) {
+        return getValue() - other.getValue();
+    }
+
+    public static ArrayList<Card> getShuffledDeck() {
+        ArrayList<Card> deck = makeDefaultCards();
+        PrimitiveIterator.OfInt ints = new Random().ints(0, deck.size() - 1).iterator();
+        for (int count = 0; count < Math.max(deck.size(), 75); ++count) {
+            Card card = deck.remove(0);
+            deck.add(ints.nextInt(), card);
+        }
+
+        return deck;
+    }
+
+    public static ArrayList<Card> makeDefaultCards() {
+        String[] symbols = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"};
+        int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+
+        // Run with JVM option '-ea'
+        assert symbols.length == values.length;
+
+        ArrayList<Card> cards = new ArrayList<>();
+        for (int cc = 0; cc < symbols.length; ++cc) {
+            cards.add(new Card(symbols[cc], values[cc]));
+        }
+
+        return cards;
+    }
+}
--- Miscellaneous/src/P7_Dhruva_Krupa_SelectionSort.java
+++ Miscellaneous/src/P7_Dhruva_Krupa_SelectionSort.java
@@ -1,3 +1,17 @@
+/*
+ * Name: Krupa Dhruva
+ * Date: January 10, 2021
+ * Period: 7
+ * Time Taken: 45 minutes
+ *
+ * Lab Reflection:
+ * I enjoyed working on this lab and experimenting with the selection
+ * sorting technique. I developed a better understanding of yet
+ * another type of sorting and I also learned how to come up with
+ * the logic for an algorithm; the thought process. Approaching this
+ * as a fun puzzle really helped me better understand and enjoy the lab!
+ */
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class P7_Dhruva_Krupa_SelectionSort {
@@ -37,13 +51,39 @@ public class P7_Dhruva_Krupa_SelectionSort {
         }
     }
 
-    public static void main(String[] args) {
-        int[] num = {5, 7, 2, 4, 3, 9};
-        System.out.println("Starting array: " + Arrays.toString(num));
-        selectionSort1(num);
+    public static void selectionSort(ArrayList<Card> cards) {
+        for (int pass = 0; pass < cards.size() - 1; ++pass) {
+            int indexOfMax = 0;
 
-        String[] data = {"eat", "steaks", "juicy", "huge", "dogs", "big"};
-        System.out.println("Starting array: " + Arrays.toString(data));
-        selectionSort2(data);
+            for (int ii = 1; ii < cards.size() - pass; ++ii) {
+                if (cards.get(ii).compareTo(cards.get(indexOfMax)) > 0) {
+                    indexOfMax = ii;
+                }
+            }
+
+            Card tmp = cards.get(cards.size() - 1 - pass);
+            cards.set(cards.size() - 1 - pass, cards.get(indexOfMax));
+            cards.set(indexOfMax, tmp);
+
+            System.out.println(cards);
+        }
+    }
+
+    public static void main(String[] args) {
+        //        int[] num = {5, 7, 2, 4, 3, 9};
+        //        System.out.println("Starting array: " + Arrays.toString(num));
+        //        selectionSort1(num);
+        //
+        //        System.out.println();
+        //
+        //        String[] data = {"eat", "steaks", "juicy", "huge", "dogs", "big"};
+        //        System.out.println("Starting array: " + Arrays.toString(data));
+        //        selectionSort2(data);
+        //
+        //        System.out.println();
+        //
+        //        ArrayList<Card> cards = Card.getShuffledDeck();
+        //        System.out.println("Starting Deck: " + cards);
+        //        selectionSort(cards);
     }
 }
