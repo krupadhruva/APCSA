812f8d5ffb470ec040f28d38c3665c625892d1e4
Krupa Dhruva <krupadhruva@gmail.com>
2021-03-07 21:56:59 -0800

    Escape Room Lab 2021: Added reflection

--- P7_Dhruva_Krupa_EscapeRoom/src/WizardLabDriver.java
+++ P7_Dhruva_Krupa_EscapeRoom/src/WizardLabDriver.java
@@ -1,3 +1,20 @@
+/*
+ * Name: Krupa Dhruva
+ * Date: March 07, 2021
+ * Period: 7
+ * Time Taken: 80 minutes
+ *
+ * Lab Reflection:
+ * The first part of the lab involved understanding the game and the following
+ * the instructions closely. It was easy to make mistakes and so I had to pay extra
+ * attention to follow every detail. I developed a high level understanding of interface
+ * and abstract methods.
+ * Implementing lookup of items in a list honoring order rules was challenging. I
+ * learned an approach to implement it and would like to explore other alternatives.
+ * I was able to semi-automate running the game by sending inputs from a file. With this,
+ * I was able to cover more alternatives. Overall, the game was interesting and enjoyable.
+ */
+
 public class WizardLabDriver {
 
     public static void main(String[] args) {
